a4c6c9c1832247c1097b791b7ea83b366f859f6f
den0206 <mac.pop@mineo.jp>
2020-04-15 19:53:26 +0900

    Initial Commit

---
+++ You Tube -app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Tube -app
+//
+//  Created by 酒井ゆうき on 2020/04/15.
+//  Copyright © 2020 Yuuki sakai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
